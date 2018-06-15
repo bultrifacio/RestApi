f77f65fde91e94b8fe721536ffb24acefb72cbc4
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-06-15 22:20:58 +0200

    First commit

---
+++ src/main/java/com/arroyo/carlos/juan/RestApiTheamApplication.java
@@ -0,0 +1,12 @@
+package com.arroyo.carlos.juan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTheamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTheamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arroyo/carlos/juan/RestApiTheamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arroyo.carlos.juan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestApiTheamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
