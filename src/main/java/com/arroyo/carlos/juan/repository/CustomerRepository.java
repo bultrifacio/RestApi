21c798517e1867e950cd58e297e224303db65dc9
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-06-17 18:59:44 +0200

    feat:added CustomerRepository

---
+++ src/main/java/com/arroyo/carlos/juan/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.arroyo.carlos.juan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arroyo.carlos.juan.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+}
