aeba4387c427e9158413cf940eb8d5800db7f840
Juan Carlos Arroyo Herrera <jcah20022@gmail.com>
2018-06-27 20:50:41 +0200

    feat: changed the code of controllers to services for more legibility.

--- src/main/java/com/arroyo/carlos/juan/controllers/CustomerController.java
+++ src/main/java/com/arroyo/carlos/juan/controllers/CustomerController.java
@@ -1,7 +1,6 @@
 package com.arroyo.carlos.juan.controllers;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,69 +11,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arroyo.carlos.juan.model.Customer;
-import com.arroyo.carlos.juan.repository.CustomerRepository;
-import com.arroyo.carlos.juan.repository.UserRepository;
+import com.arroyo.carlos.juan.services.CustomerService;
 
 @RestController
 public class CustomerController {
-
-	private CustomerRepository customerRepository;
-	private Customer customer;
-	private UserRepository userRepository;
-
+	
+	private final CustomerService customerService;
+	
 	@Autowired
-	public CustomerController(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
+	public CustomerController(CustomerService customerService) {
+		this.customerService = customerService;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/customers")
 	public List<Customer> getCustomers() {
-		return new ArrayList<>(customerRepository.findAll());
+		return customerService.getCustomers();
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/customers/{id}")
 	public Customer getCustomer(@PathVariable Long id) {
-		return customerRepository.findById(id).get();
+		return customerService.getCustomer(id);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/customers")
-	public boolean createCustomer(@RequestBody Customer customer, Principal principal) {
-		Long creatorUserId = userRepository.findUserByUsername(principal.getName()).getId();
-		if (customerRepository.findById(customer.getId()) == null) {
-			this.customer = new Customer(customer.getName(), customer.getSurname(), customer.getPhotoUrl(),
-					creatorUserId, creatorUserId);
-			customerRepository.save(this.customer);
-			return true;
-		}
-		return false;
+	public void createCustomer(@RequestBody Customer customer, Principal principal) {
+		customerService.createCustomer(customer, principal);
+	}
 
+	@RequestMapping(method = RequestMethod.PATCH, value = "/customers/{id}")
+	public void updateCustomer(@RequestBody Customer customer, @PathVariable Long id, Principal principal) {
+		customerService.updateCustomer(customer, principal, id);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/customers/{id}")
-	public boolean deleteCustomer(@PathVariable Long id) {
-		if (customerRepository.findById(id) != null) {
-			customerRepository.deleteById(id);
-			return true;
-		}
-		return false;
+	public void deleteCustomer(@PathVariable Long id) {
+		customerService.deleteCustomer(id);
 	}
-
-	@RequestMapping(method = RequestMethod.PUT, value = "/customers/{id}")
-	public boolean updateCustomer(@RequestBody Customer customer, @PathVariable Long id, Principal principal) {
-		Long lastModificationUserId = userRepository.findUserByUsername(principal.getName()).getId();
-		if (customerRepository.findById(id) != null) {
-			this.customer = customerRepository.findById(id).get();
-			if (customer.getName() != null && customer.getName() != this.customer.getName())
-				this.customer.setName(customer.getName());
-			if (customer.getSurname() != null && customer.getSurname() != this.customer.getSurname())
-				this.customer.setSurname(customer.getSurname());
-			if (customer.getPhotoUrl() != null && customer.getPhotoUrl() != this.customer.getPhotoUrl())
-				this.customer.setPhotoUrl(customer.getPhotoUrl());
-			this.customer.setLastModificationUserId(lastModificationUserId);
-			customerRepository.save(this.customer);
-			return true;
-		}
-		return false;
-	}
-
 }
--- src/main/java/com/arroyo/carlos/juan/controllers/UserController.java
+++ src/main/java/com/arroyo/carlos/juan/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.arroyo.carlos.juan.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,63 +9,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arroyo.carlos.juan.model.Role;
 import com.arroyo.carlos.juan.model.User;
-import com.arroyo.carlos.juan.repository.UserRepository;
+import com.arroyo.carlos.juan.services.UserService;
 
 @RestController
 public class UserController {
 
-	private UserRepository userRepository;
-	private User user;
+	private final UserService userService;
 
 	@Autowired
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/users")
 	public List<User> getUsers() {
-		return new ArrayList<>(userRepository.findAll());
+		return userService.getUsers();
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/users/{id}")
 	public User getUser(@PathVariable Long id) {
-		return userRepository.findById(id).get();
+		return userService.getUser(id);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/users")
 	public void createUser(@RequestBody User user) {
-		this.user = null;
-		this.user.setRole(user.isAdmin());
-		this.user.setUsername(user.getUsername());
-		this.user.setPassword(user.getPassword());
-		userRepository.save(this.user);
+		userService.createUser(user);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/users/{id}")
+	public void updateUser(@RequestBody User user, @PathVariable Long id) {
+		userService.updateUser(user, id);
+	}
+
+	@RequestMapping(method = RequestMethod.PATCH, value = "/users/roles/add/{id}")
+	public void addRoles(@RequestBody Role role, @PathVariable Long id) {
+		userService.addRole(role, id);
+	}
+
+	@RequestMapping(method = RequestMethod.PATCH, value = "/users/roles/remove/{id}")
+	public void removeRole(@RequestBody Role role, @PathVariable Long id) {
+		userService.removeRole(role, id);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/users/{id}")
-	public boolean deleteUser(@PathVariable Long id) {
-		if (userRepository.findById(id) != null) {
-			userRepository.deleteById(id);
-			return true;
-		}
-		return false;
-
-	}
-
-	@RequestMapping(method = RequestMethod.PATCH, value = "/users/{id}")
-	public boolean updateUser(@PathVariable Long id, @RequestBody User user) {
-		if (userRepository.findById(id) != null) {
-			this.user = userRepository.findById(id).get();
-			if (user.getUsername() != null && user.getUsername() != this.user.getUsername())
-				this.user.setUsername(user.getUsername());
-			if (user.isAdmin() != this.user.isAdmin())
-				this.user.setRole(user.isAdmin());
-			if (user.getPassword() != null && user.getPassword() != this.user.getPassword())
-				this.user.setPassword(user.getPassword());
-			userRepository.save(this.user);
-			return true;
-		}
-		return false;
+	public void deleteUser(@PathVariable Long id) {
+		userService.deleteUser(id);
 	}
 
 }
